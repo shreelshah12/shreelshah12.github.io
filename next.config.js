8fa1758a6805d12395b1c427e847fc19bcc4b064
Shreel Shah <shreelshah12@Shreels-MacBook-Pro-1119.local>
2024-05-23 19:59:18 -0400

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
-const path = require('path')
+/*const path = require('path')
  
-/*module.exports = {
+module.exports = {
   sassOptions: {
     includePaths: [path.join(__dirname, 'styles')],
   },
