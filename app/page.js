f47cf8c8e1e7b46a43c678fd1457446b7b580097
Shreel Shah <shreelshah12@Shreels-MacBook-Pro-1119.local>
2024-05-23 20:04:12 -0400

    removing all blog references

--- app/page.js
+++ app/page.js
@@ -1,6 +1,6 @@
 import { personalData } from "@/utils/data/personal-data";
 import AboutSection from "./components/homepage/about";
-import Blog from "./components/homepage/blog";
+//import Blog from "./components/homepage/blog";
 import Education from "./components/homepage/education";
 import Experience from "./components/homepage/experience";
 import HeroSection from "./components/homepage/hero-section";
@@ -22,7 +22,7 @@ async function getData() {
 };
 
 export default async function Home() {
-  const blogs = await getData();
+  //const blogs = await getData();
 
   return (
     <>
