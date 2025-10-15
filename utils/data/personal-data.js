1a4daa70b2781d3174a6d9421543bc192663b2f7
shreelshah12 <shreelshah12@gmail.com>
2025-10-14 22:09:17 -0400

    Update personal-data.js

--- utils/data/personal-data.js
+++ utils/data/personal-data.js
@@ -1,7 +1,7 @@
 export const personalData = {
   name: "Shreel",
   profile: '/IMG_4693.jpeg',
-  designation: " Cloud & Data Engineer ",
+  designation: " Cloud Infrastructure Engineer ",
   description: "I'm Shreel Shah, an engineer at Databricks. My main expertise revolves around designing enterprise-level microservice applications and API's. I am also working to develop complex ETL data pipelines, data warehousing solutions, AI/ML applications, and cloud infrastructures.",
   description2: "My passions and interests lie within the realms of developing full-stack applications, scaling use cases for AI/ML, and driving impact through software.",
   description3: "Always open to building something new and exciting. Reach out and let's work to make something impactful!",
